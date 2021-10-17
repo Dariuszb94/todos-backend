aba0afcac66e5ae669840f04e989d282c5e65dbe
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-10-17 12:18:31 +0200

    404

--- index.js
+++ index.js
@@ -12,10 +12,12 @@ app.use(bodyParser.json());
 
 app.use("/todos", todosRoutes);
 app.get("/", (req, res) => res.send("Welcome to the todos API!"));
-// app.all("*", (req, res) =>
-//   res.send("You've tried reaching a route that doesn't exist.")
-// );
-
+app.all("*", (req, res) =>
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find url on this server!`,
+  })
+);
 app.listen(PORT, () =>
   console.log(`Server running on port: http://localhost:${PORT}`)
 );
